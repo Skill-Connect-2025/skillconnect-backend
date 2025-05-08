06f63ec21ae358200728f94a7a5a84d1fdf69b98
Birkity <birkity.yishak.m@gmail.com>
2025-05-08 12:13:38 +0300

    fix: correct import for django-environ to resolve module error

--- skillconnect/settings.py
+++ skillconnect/settings.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
-from django_environ import Env
+from environ import Env 
+
 
 # Build paths inside the project
 BASE_DIR = Path(__file__).resolve().parent.parent
