791d20c6d9a3699f9c875b19918b6d0bff33be38
Birkity <birkity.yishak.m@gmail.com>
2025-05-08 11:36:27 +0300

    Configure settings and urls

--- skillconnect/urls.py
+++ skillconnect/urls.py
@@ -1,22 +1,22 @@
-"""
-URL configuration for skillconnect project.
+from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path
+schema_view = get_schema_view(
+    openapi.Info(
+        title="SkillConnect API",
+        default_version='v1',
+        description="API for SkillConnect platform",
+    ),
+    public=True,
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('users/', include('apps.users.urls')),
+    path('jobs/', include('apps.jobs.urls')),
+    path('payments/', include('apps.payments.urls')),
+    path('disputes/', include('apps.disputes.urls')),
+    path('notifications/', include('apps.notifications.urls')),
+    path('recommendations/', include('apps.recommendations.urls')),
 ]
