806304c9a6a533a9d92628eaec84a0f908659628
Birkity <birkity.yishak.m@gmail.com>
2025-05-09 12:17:42 +0300

    Update models code

--- apps/users/models.py
+++ apps/users/models.py
@@ -2,14 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    USER_TYPE_CHOICES = (
-        ('client', 'Client'),
-        ('worker', 'Worker'),
-        ('admin', 'Admin'),
-    )
-    user_type = models.CharField(max_length=10, choices=USER_TYPE_CHOICES, default='client')
     phone_number = models.CharField(max_length=15, blank=True, null=True, unique=True)
-    location = models.CharField(max_length=100, blank=True, null=True)
     email = models.EmailField(blank=True, null=True, unique=True)
 
     def __str__(self):
@@ -19,6 +12,28 @@ class User(AbstractUser):
         verbose_name = "User"
         verbose_name_plural = "Users"
 
+    @property
+    def is_client(self):
+        return hasattr(self, 'client')
+
+    @property
+    def is_worker(self):
+        return hasattr(self, 'worker')
+
     @classmethod
     def get_by_identifier(cls, identifier):
         return cls.objects.filter(models.Q(email=identifier) | models.Q(phone_number=identifier)).first()
+
+class Client(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='client')
+    
+
+    def __str__(self):
+        return f"Client: {self.user.username}"
+
+class Worker(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='worker')
+    location = models.CharField(max_length=100, blank=True, null=True)  
+
+    def __str__(self):
+        return f"Worker: {self.user.username}"
