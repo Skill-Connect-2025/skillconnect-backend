c70d1a56888d03fc3d2cff28866b92191d2d6c4b
Birkity <birkity.yishak.m@gmail.com>
2025-05-09 22:07:11 +0300

    minor edits

--- skillconnect/settings.py
+++ skillconnect/settings.py
@@ -21,6 +21,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'api-skillconnect.wisewaytech.com']
 # Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
+    'django.contrib.sites', 
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
