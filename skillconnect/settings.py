a87ff67bdfab40b7b9168cf043577063b1c6cc17
Birkity <birkity.yishak.m@gmail.com>
2025-05-13 13:06:36 +0300

    minor edits

--- skillconnect/settings.py
+++ skillconnect/settings.py
@@ -115,5 +115,3 @@ EMAIL_USE_TLS = env.bool('EMAIL_USE_TLS', default=True)
 EMAIL_HOST_USER = env('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD', default='')
 DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL', default='SkillConnect <lily.yishak2@gmail.com>')
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
