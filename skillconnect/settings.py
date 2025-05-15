ed74a47224f2c048c9433bc9389fe03357f3f731
Peneal Aragaw <99356695+penealfa@users.noreply.github.com>
2025-05-15 15:31:10 +0300

    Update settings.py

--- skillconnect/settings.py
+++ skillconnect/settings.py
@@ -90,11 +90,11 @@ USE_TZ = True
 AUTH_USER_MODEL = 'users.User'
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),  # if you have a 'static' folder in app
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),  # if you have a 'static' folder in app
+# ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, "public/static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
