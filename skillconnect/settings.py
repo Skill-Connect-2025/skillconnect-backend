b4356b27fcfd3b705a91bd98de45802bd3cd1d09
Peneal Aragaw <99356695+penealfa@users.noreply.github.com>
2025-05-15 15:14:44 +0300

    Update settings.py

--- skillconnect/settings.py
+++ skillconnect/settings.py
@@ -90,6 +90,9 @@ USE_TZ = True
 AUTH_USER_MODEL = 'users.User'
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
